02b9871fe91060d67266370a9e587559617ea610
Suein Choi <48196612+sueinchoi@users.noreply.github.com>
2022-07-16 22:04:59 +0900

    Delete nonmem_data.R

--- Code/nonmem_data.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-data <- read_csv('Data_tidy/nonmem_all.csv')
-head(data)
-
-data %>%
-    filter(CMT %in% c('DEPOT', 'CMS')) %>%
-    write.csv('Data_tidy/nonmem_cms.csv', row.names=F)
