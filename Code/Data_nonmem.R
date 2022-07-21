f751af2ad1c0c0c7185c6de469d4876ee16a3410
sueinchoi <mychloe00@gmail.com>
2022-07-21 11:32:16 +0900

    update

--- Code/Data_nonmem.R
+++ Code/Data_nonmem.R
@@ -1,13 +1,19 @@
 library(tidyverse)
-data <- read_csv('Data_tidy/nonmem_all.csv')
+data <- read_csv('Data_tidy/nonmem_all_removed.csv', na = ".")
+data %>%
+    head(5)
+data <- data %>%
+    mutate(RATE = ifelse(MDV == 1, AMT/DUR, NA))
 
 cmt <- data %>%
     pull(CMT) %>%
     unique()
 cmt
-for(i in 2:length(cmt)){
-    data %>%
-    filter(CMT == cmt[1] | CMT == cmt[i]) %>%
-    write.csv(paste0('Data_tidy/nonmem_', cmt[i], '.csv'), row.names = F)
-}
 
+for(i in 1:(length(cmt)-1)){
+    data %>%
+    filter(CMT == cmt[6] | CMT == cmt[i]) %>%
+    select(-ADDL_diff, -DATETIME) %>%
+    write.csv(paste0('Data_tidy/nonmem_', cmt[i], '.csv'), row.names = F, na = ".")
+}
+?write.csv
